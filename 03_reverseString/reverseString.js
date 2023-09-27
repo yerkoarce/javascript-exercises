a8c6a8021baf043fe84bc85a28c84ea22ea26696
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-27 19:04:41 -0300

    exercise 3 ok

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    let reverseArr = [];
+    for (const leter of arr){
+        reverseArr.unshift(leter);
+    }
+    let newString = reverseArr.join('');
+    return newString;
 };
 
 // Do not edit below this line
