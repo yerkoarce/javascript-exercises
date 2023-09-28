9cc3d7e21c3200925bcb3eadb830db22048deed7
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-27 22:43:13 -0300

    exercise 4 ok

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array , ...args) {
+    let newArray = [];
+    array.forEach((item) => {    // por cada elemento item en array se ejecuta el codigo
+        if (!args.includes(item)){ //si item no esta en args 
+            newArray.push(item);   //lo agrega a la nueva array
+        }
+    });
 
+    return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
