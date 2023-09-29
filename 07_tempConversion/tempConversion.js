7de0461839ee2f4c058f3b62ceae9b80d53d9af2
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-28 23:04:46 -0300

    excercise 7 ok, complete foundations 4 (y)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = ((temp - 32) * (5 / 9));
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let far = (temp * (9 / 5) + 32);
+  return Number(far.toFixed(1));
 };
 
 // Do not edit below this line
