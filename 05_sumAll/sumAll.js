6e62bb762548e77807ced0ad14128aa5cfa28c21
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-28 21:52:18 -0300

    exercise 5 ok, puede mejorarse

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a , b) {
+    if (typeof a !== 'number' || typeof b !== 'number'){
+        return 'ERROR';
+    }
+    let sum = 0;
+    
+    if (a < b){
+        for (let i = a ; i <= b ; i++){
+            sum += i;
+        }
+        if (sum < 0){
+            return 'ERROR';
+        }
+        return sum;
+    }else if (b < a){
+        for (let i = b ; i <= a ; i++){
+            sum += i;
+        }
+        if (sum < 0){
+            return 'ERROR';
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
