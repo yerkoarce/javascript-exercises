b5248beea67c593e48285a460bf5e9c2d9d3ca50
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-28 22:45:59 -0300

    excercise 6 ok

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    if ((year % 100 == 0) && (year % 400 == 0)){
+        return true;
+    }
+    
+    if (year % 100 === 0) return false;
+    if (year % 4 === 0) return true;
+    return false;
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
